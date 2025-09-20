52c2d5358cdf681111a62df226b53eca00486979
SchulzeLS <schulzels1501@gmail.com>
2025-09-20 18:30:19 -0300

    Update CID-10 SQL

--- migration/CID-1.sql
+++ migration/CID-1.sql
@@ -1,8 +1,8 @@
 --- Criação da tabela CID-10
-CREATE TABLE if NOT EXISTS CID-10 (
+CREATE TABLE if NOT EXISTS CID_10 (
   cid UUID PRIMARY KEY,
   descricaoCid TEXT
 );
-
+-- Exemplo: inserir dados iniciais
 INSERT INTO CID-1 (descricaoCid) VALUES 
     ('Carcer');
